2975cd3f67dfe9b639c89d3f04a2fcb6cbc0ccdb
500kguser <102877038+500kguser@users.noreply.github.com>
2022-06-08 14:37:11 +0100

    Python Arithmetic Operators
    Python Arithmetic Operators

---
+++ Arithmetic operators.py
@@ -0,0 +1,8 @@
+print("All Examples use 10 (A) and 10 (B). It converts the number into a string for concatenation purposes.")
+print("Addition = " + str(10 + 10))         #Adds the values together
+print("Subtraction = " + str(10 - 10))      #Subtracts the first integer from the second
+print("Multiplication = " + str(10 * 10))   #Multiplies the first and second integer
+print("Exponentiation = " + str(10 ** 10))  #Multiplies the first integer to the power of the second
+print("Division = " + str(10 / 10))         #Divides the first and second integer
+print("Floor Division = " + str(10 // 10))  #Division that results into whole number with no decimal places
+print("Modulo = " + str(10 % 10))           #The remainder after the division between the first and second integer
