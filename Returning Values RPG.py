2c43fb33b0e969075195062eba7daba89257fcb0
ptd3v <tovey@live.co.uk>
2022-06-20 22:00:58 +0100

    Update to RPG

--- Returning Values RPG.py
+++ Returning Values RPG.py
@@ -4,7 +4,7 @@
 def hit_points(seconds):
   hours = seconds // 3600
   minutes = (seconds - hours * 3600) // 60
-  remaining_seconds = seconds - hours * 3600 - minutes * 60
+  remaining_seconds = seconds - hours * 3600 - minutes * 30
   return hours, minutes, remaining_seconds
 
 hours, minutes, seconds = hit_points(4000)
