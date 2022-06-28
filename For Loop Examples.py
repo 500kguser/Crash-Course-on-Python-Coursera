56005125ff0eee315b53b2b04beb24c5efbfc2f4
500kguser <102877038+500kguser@users.noreply.github.com>
2022-06-28 14:48:14 +0100

    Update For Loop Examples.py

--- For Loop Examples.py
+++ For Loop Examples.py
@@ -1,2 +1,15 @@
+#Numbers are always one less than expected
 for x in range(5):
     print(x)
+
+#For Loops Intro
+def square(n):
+    return n*n
+
+def sum_squares(x):
+    sum = 0
+    for n in range(x):
+        sum += 1
+    return x
+
+print(sum_squares(10)) # Should be 285
