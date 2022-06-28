6c67e8b5060564446f1b01569b310f1c5cf7597d
500kguser <102877038+500kguser@users.noreply.github.com>
2022-06-28 13:15:59 +0100

    Update Practical Exam - Loops.py

--- Practical Exam - Loops.py
+++ Practical Exam - Loops.py
@@ -1,3 +1,5 @@
+
+#Question Two
 def print_prime_factors(number):
   # Start with two, which is the first prime
   factor = 2
@@ -10,9 +12,28 @@ def print_prime_factors(number):
       number = number / factor
     else:
       # If it's not, increment the factor by one
-      factor = factor + 1
+      factor = factor + 1 #Solution
   return "Done"
 
 print_prime_factors(100)
 # Should print 2,2,5,5
-# DO NOT DELETE THIS COMMENT
+
+#Question 3:
+def is_power_of_two(n):
+    # Check if the number can be divided by two without a remainder
+    while n % 2 == 0:
+        if n <= 0: #Solution
+            return False
+        n = n / 2
+    # If after dividing by two the number is 1, it's a power of two
+    if n == 1:
+        return True
+    return False
+
+
+print(is_power_of_two(0))  # Should be False
+print(is_power_of_two(1))  # Should be True
+print(is_power_of_two(8))  # Should be True
+print(is_power_of_two(9))  # Should be False
+
+#Question 4:
