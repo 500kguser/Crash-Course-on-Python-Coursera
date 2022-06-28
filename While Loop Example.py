672436bc699fac27a47f9472323351ff463d84f3
500kguser <102877038+500kguser@users.noreply.github.com>
2022-06-28 12:23:41 +0100

    Update While Loop Example.py

--- While Loop Example.py
+++ While Loop Example.py
@@ -28,3 +28,12 @@ def count_down(start_number):
   print("Zero!")
 
 count_down(3)
+
+def print_range(start, end):
+	# Loop through the numbers from start to end
+	n = start
+	while n <= end:
+		print(n)
+		n = n + 1 #Added Line
+
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
