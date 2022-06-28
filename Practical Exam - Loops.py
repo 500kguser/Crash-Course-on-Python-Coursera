d91c23128d8ceeb7b98bc1dcac3443a86f47afbf
500kguser <102877038+500kguser@users.noreply.github.com>
2022-06-28 13:28:44 +0100

    Update Practical Exam - Loops.py

--- Practical Exam - Loops.py
+++ Practical Exam - Loops.py
@@ -37,3 +37,23 @@ print(is_power_of_two(8))  # Should be True
 print(is_power_of_two(9))  # Should be False
 
 #Question 4:
+def sum_divisors(n):
+  sum = 0
+  z = 1 #Solution Start
+  while n > z:
+        if n % z == 0:
+            sum = sum + z
+            z = z + 1
+        else:
+            z = z + 1 #Solution End
+  # Return the sum of all divisors of n, not including n
+  return sum
+
+print(sum_divisors(0))
+# 0
+print(sum_divisors(3)) # Should sum of 1
+# 1
+print(sum_divisors(36)) # Should sum of 1+2+3+4+6+9+12+18
+# 55
+print(sum_divisors(102)) # Should be sum of 2+3+6+17+34+51
+# 114
