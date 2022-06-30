280923b9bcd2c89e12b083d2b17135bb6fc64a2f
500kguser <102877038+500kguser@users.noreply.github.com>
2022-06-30 14:34:10 +0100

    Update For Loop Examples.py

--- For Loop Examples.py
+++ For Loop Examples.py
@@ -9,7 +9,7 @@ def square(n):
 def sum_squares(x):
     sum = 0
     for n in range(x):
-        sum += 1
-    return x
+        sum += square(n)
+    return sum
 
 print(sum_squares(10)) # Should be 285
