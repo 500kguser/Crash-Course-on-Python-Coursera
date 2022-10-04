2028ccaa45f23b206645db8441205330d6325230
500kguser <102877038+500kguser@users.noreply.github.com>
2022-10-04 15:03:42 +0100

    Update Basic String Operations.py

--- Basic String Operations.py
+++ Basic String Operations.py
@@ -1,3 +1,7 @@
 astring = "Hello world!"
 print(astring[::-1])
 
+#This prints the characters of string from 3 to 7 skipping one character.
+astring = "Hello world!"
+print(astring[3:7])
+print(astring[3:7:1])
