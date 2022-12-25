5bbeb7f51edd7a6be02fcbe30f9b54cdcc88f11a
ptd3v <102877038+ptd3v@users.noreply.github.com>
2022-12-25 17:44:23 +0100

    Update Arithmetic operators.py

--- Arithmetic operators.py
+++ Arithmetic operators.py
@@ -5,4 +5,4 @@ print("Multiplication = " + str(10 * 10))   #Multiplies the first and second int
 print("Exponentiation = " + str(10 ** 10))  #Multiplies the first integer to the power of the second
 print("Division = " + str(10 / 10))         #Divides the first and second integer
 print("Floor Division = " + str(10 // 10))  #Division that results into whole number with no decimal places
-print("Modulo = " + str(10 % 10))           #The remainder after the division between the first and second integer
+print("Modulo = " + str(10 % 11))           #The remainder after the division between the first and second integer
