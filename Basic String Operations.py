7d58e55243dd94f9c70b6b42d990cf26ccd88fbe
ptd3v <tovey@live.co.uk>
2023-02-25 11:33:06 +0000

    Update Basic String Operations.py

--- Basic String Operations.py
+++ Basic String Operations.py
@@ -3,5 +3,5 @@ print(astring[::-1])
 
 #This prints the characters of string from 3 to 7 skipping one character.
 astring = "Hello world!"
-print(astring[3:7]
+print(astring[3:7])
 print(astring[3:7:1])
