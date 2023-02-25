fe5c3977274264d49c527bd362e16aecb719b0c9
ptd3v <tovey@live.co.uk>
2023-02-25 13:42:52 +0000

    Update Hello World x10 (C#).cs

--- Hello World x10 (C#).cs
+++ Hello World x10 (C#).cs
@@ -3,3 +3,12 @@ for (int i = 0; i < 10; i++)
 {
     Console.WriteLine("Hello");
 }
+
+#C
+for (int i = 0; i < 10; i++) {
+  printf("Hello\n");
+  }
+
+ #C++
+for (int i = 0; i < 10; ++i)
+  cout << "Hello\n";
