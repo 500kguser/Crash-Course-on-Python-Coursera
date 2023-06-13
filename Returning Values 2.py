10280901d974135ca78cdba1098252228ab48159
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-06-13 21:12:06 +0100

    Example Ping

--- Returning Values 2.py
+++ Returning Values 2.py
@@ -1,5 +1,5 @@
 #Use the get_seconds function to work out the amount of seconds in 2 hours and 30 minutes..
-#then add this number to the amount of seconds in 45 minutes and 15 seconds.
+#Then add this number to the amount of seconds in 45 minutes and 15 seconds.
 
 def convert_seconds(seconds):
   hours = seconds // 3600
